6072f657ed94ca96323847a400b4135607171dbb
Deb Johnson <friendswithdrj@gmail.com>
2022-09-30 17:35:20 -0500

    Change to use playlists from db

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -18,6 +18,7 @@ import {
   MdFavorite,
   MdPlaylistAdd,
 } from "react-icons/md";
+import { usePlaylist } from "../lib/hooks";
 
 const navMenu = [
   {
@@ -50,9 +51,10 @@ const musicMenu = [
   },
 ];
 
-const playlists = new Array(30).fill(1).map((_, i) => `Playlist ${i}`);
+// const playlists = new Array(30).fill(1).map((_, i) => `Playlist ${i}`);
 
 const Sidebar = () => {
+  const { playlists } = usePlaylist();
   return (
     <Box
       width="100%"
@@ -110,10 +112,10 @@ const Sidebar = () => {
         <Box height="54%" overflowY="auto" paddingY="20px">
           <List spacing={2}>
             {playlists.map((playlist) => (
-              <ListItem paddingX="10px" fontSize="16px" key={playlist}>
+              <ListItem paddingX="10px" fontSize="16px" key={playlist.id}>
                 <LinkBox>
                   <NextLink href="/" passHref>
-                    <LinkOverlay>{playlist}</LinkOverlay>
+                    <LinkOverlay>{playlist.name}</LinkOverlay>
                   </NextLink>
                 </LinkBox>
               </ListItem>
